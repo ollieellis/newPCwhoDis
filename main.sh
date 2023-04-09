6c7f51b367e7c9393d83aeefabd87f928fb28651
ollieellis <ollieellis1@outlook.com>
2023-04-09 12:12:25 +0100

    going offline

---
+++ main.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "export PATH=/opt/homebrew/bin:$PATH" >> .zshrc
